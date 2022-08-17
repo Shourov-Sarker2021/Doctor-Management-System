c0f0b19d3848a03451983d16034287ea81db7c2b
Shourov Sarker <80618479+Shourov-Sarker2021@users.noreply.github.com>
2022-08-17 15:37:57 +0600

    jkk

---
+++ database/migrations/2022_08_15_060229_create_doctorss_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('email',50);
+            $table->string('phone');
+            $table->string('education');
+            $table->string('specialist');
+            $table->string('experence');
+            $table->text('details')->nullable();
+            $table->string('image',255);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
